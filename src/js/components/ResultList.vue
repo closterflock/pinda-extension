25d7ce84ee86aa4e9fb358713dcd8f4bf5173254
James Spence <Jamesaspence20@gmail.com>
2016-05-03 20:20:43 -0500

    initial components

---
+++ src/js/components/ResultList.vue
@@ -0,0 +1,37 @@
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+    <div class="result-list" v-show="links.length > 0">
+        <h2>
+            {{links.length}} Result{{links.length == 1 ? '' : 's'}}
+        </h2>
+        <ul class="results">
+            <li class="result" v-for="link in links">
+                <h3 class="title">
+                    <a v-bind:href="link.url" target="_blank">
+                        {{link.title}}
+                    </a>
+                </h3>
+                <p v-if="link.description && link.description !== ''" class="description">
+                    {{link.description}}
+                </p>
+                <ul class="tag-list">
+                    <li class="tag" v-for="tag in link.tags">
+                        {{tag.name}}
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'links'
+        ],
+        methods: {
+            showResults: function (links) {
+                this.links = links;
+            }
+        }
+    }
+</script>
---
+++ src/js/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <div class="search-bar">
+        <label>
+            Search
+            <input v-on:keyup="onKeyUp | debounce 500" v-model="term" type="text" name="search"/>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'onTermChange'
+        ],
+        data: function () {
+            return {
+                term: ''
+            };
+        },
+        methods: {
+            onKeyUp: function () {
+                if (this.term !== '') {
+                    this.onTermChange(this.term);
+                }
+            }
+        }
+    }
+</script>
