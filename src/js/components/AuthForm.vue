bd2e4f06962d1cc384ac3c75939667651c80cdc5
James Spence <Jamesaspence20@gmail.com>
2016-05-18 23:37:02 -0500

    AuthForm vue component

---
+++ src/js/components/AuthForm.vue
@@ -0,0 +1,41 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <form class="auth-form" v-on:submit.prevent="onSubmit">
+        <h2>
+            Sign {{isRegistration ? 'Up' : 'In'}}
+        </h2>
+        <label>
+            Email Address
+            <input type="text" name="email" v-model="email" lazy/>
+        </label>
+        <label>
+            Password
+            <input type="password" name="password" v-model="password" lazy/>
+        </label>
+        <label>
+            New here? Sign up
+            <input type="checkbox" v-model="isRegistration"/>
+        </label>
+        <div class="register-inputs" v-show="isRegistration">
+            <label>
+                Confirm Password
+                <input type="password" name="confirm" v-model="confirm" lazy/>
+            </label>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                isRegistration: false
+            }
+        },
+        methods: {
+            onSubmit: function (e) {
+                console.log('submitted!');
+            }
+        }
+    }
+</script>
