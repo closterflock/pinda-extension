5eb063886f99438d4e7cdec0138041cf348fb962
James Spence <Jamesaspence20@gmail.com>
2016-09-03 12:57:29 -0500

    moved Header out of global directory, added callbacks

--- src/js/react/Application.jsx
+++ src/js/react/Application.jsx
@@ -2,14 +2,20 @@
 // @flow
 
 var React = require('react');
-var Header = require('./global/Header.jsx');
+var Header = require('./Header.jsx');
 
 module.exports = React.createClass({
+    onBackButton: function () {
+
+    },
+    onNavButton: function () {
+
+    },
     render: function () {
         return (
             <div className="app">
-                <Header/>
-                <h2>Application</h2>
+                <Header onBackButton={this.onBackButton} onNavButton={this.onNavButton}/>
+
             </div>
         );
     }
--- src/js/react/Header.jsx
+++ src/js/react/Header.jsx
@@ -4,11 +4,18 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    propTypes: {
+        onBackButton: React.PropTypes.func.isRequired,
+        onNavButton: React.PropTypes.func.isRequired
+    },
     getInitialState: function () {
         return {
             backStateActive: false,
             navActive: false
         }
+    },
+    getCurrentState: function () {
+        return this.state;
     },
     backButtonShown: function () {
         return this.state.backButtonShown;
@@ -17,18 +24,24 @@ module.exports = React.createClass({
         if (typeof newState !== 'boolean') {
             newState = !this.backButtonShown();
         }
+        var state: Object = this.getCurrentState();
 
-        this.setState({
-            backStateActive: newState
-        });
+        this.props.onBackButton(newState);
+        state.backStateActive = newState;
+        this.setState(state);
     },
     navIsActive: function () {
         return this.state.navActive;
     },
-    toggleNav: function () {
-        this.setState({
-            navActive: !this.navIsActive()
-        });
+    toggleNav: function (newState: boolean) {
+        if (typeof newState !== 'boolean') {
+            newState = !this.navIsActive();
+        }
+        var state: Object = this.getCurrentState();
+
+        this.props.onNavButton(newState);
+        state.navActive = newState;
+        this.setState(state);
     },
     render: function () {
         return (
