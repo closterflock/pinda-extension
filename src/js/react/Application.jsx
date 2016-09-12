5fe8cd0f65b1df30885f4f2d18edfe6529ae0a01
James Spence <Jamesaspence20@gmail.com>
2016-09-11 22:05:42 -0500

    added component prop

--- src/js/react/Application.jsx
+++ src/js/react/Application.jsx
@@ -14,6 +14,7 @@ import Menu from './Menu.jsx'
 export default class Application extends binder(React.Component) {
     contentComponent: Content;
     authFormComponent: AuthForm;
+    headerComponent: Header;
 
     static propTypes = {
         loggedIn: React.PropTypes.bool
